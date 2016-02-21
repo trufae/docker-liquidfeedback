2285d62264c843c16c7f71ea7404ee7e61d13d84
pancake <pancake@nopcode.org>
2016-02-21 16:01:13 +0100

    Fix createdb.sql

--- createdb.sql
+++ createdb.sql
@@ -4,6 +4,7 @@ INSERT INTO contingent (polling, time_frame, text_entry_limit, initiative_limit)
 INSERT INTO contingent (polling, time_frame, text_entry_limit, initiative_limit) VALUES (true, '1 hour', 200, 60);
 INSERT INTO contingent (polling, time_frame, text_entry_limit, initiative_limit) VALUES (true, '1 day', 800, 120);
 INSERT INTO policy (index, name, admission_time, discussion_time, verification_time, voting_time, issue_quorum_num, issue_quorum_den, initiative_quorum_num, initiative_quorum_den) VALUES (1, 'Default policy', '8 days', '15 days', '8 days', '15 days', 10, 100, 10, 100);
+INSERT INTO policy (index, name, min_admission_time, max_admission_time, discussion_time, verification_time, voting_time, issue_quorum_num, issue_quorum_den, initiative_quorum_num, initiative_quorum_den) VALUES (1, 'Default policy', '4 days', '8 days', '15 days', '8 days', '15 days', 10, 100, 10, 100);
 INSERT INTO unit (name) VALUES ('Our organization');
 INSERT INTO area (unit_id, name) VALUES (1, 'Default area');
 INSERT INTO allowed_policy (area_id, policy_id, default_policy) VALUES (1, 1, TRUE);
